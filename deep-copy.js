5ba25ccd3e8b651977d8b9559f5fc47dcf1f69bd
AyamotoKohei <koh14taka2@gmail.com>
2021-11-09 23:52:13 +0900

    add: マージとコピーの学習

--- deep-copy.js
+++ deep-copy.js
@@ -4,6 +4,7 @@ const patty = {
   address: { town: 'Maple Town' },
 };
 
+// 強引な手段で DeepCopy を行う
 const rolley = JSON.parse(JSON.stringify(patty));
 rolley.name = 'Rolley Cocker';
 rolley.email = 'rolley@palm.town';
