1e2b15f7605ef7b710f095a18d32db0f97be26e6
AyamotoKohei <koh14taka2@gmail.com>
2021-11-09 21:18:36 +0900

    add: アロー関数の学習

---
+++ arrow-sample.js
@@ -0,0 +1,16 @@
+// function キーワードによる関数式
+const plusOne = function (n) {
+  return n + 1;
+}
+
+// アロー関数式
+const addOne = (n) => {
+  return n + 1;
+};
+
+// アロー関数式、さらに省略
+const increment = n => n + 1;
+
+console.log(plusOne(1));
+console.log(addOne(1));
+console.log(increment(1));
